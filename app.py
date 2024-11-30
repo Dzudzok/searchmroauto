b16c9a21ac3e35a85dff288add07f93809be7657
Dzudzok <mateuszdurczok01@gmail.com>
2024-11-30 11:00:54 +0100

    Fix app.py to include dynamic port support

--- app.py
+++ app.py
@@ -48,7 +48,6 @@ def search():
 
     return jsonify({"results": results})
 
-
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5001))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.getenv("PORT", 5000))  # Railway przekazuje port przez zmienną środowiskową
+    app.run(host='0.0.0.0', port=port)  # Aplikacja nasłuchuje na wskazanym porcie
